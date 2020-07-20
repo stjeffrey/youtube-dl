974756e5cd4ca6e28dd5b600e536bc460e477886
SJ <jeffreys@flextechservices.com>
2020-07-20 13:56:05 -0400

    Working for Idello

--- youtube_dl/extractor/idello.py
+++ youtube_dl/extractor/idello.py
@@ -1,11 +1,12 @@
 from __future__ import unicode_literals
 
+import json
 from .common import InfoExtractor
 from youtube_dl.utils import (
     compat_urllib_request,
     urlencode_postdata
 )
-import youtube_dl.utils
+
 import re
 import hashlib
 
@@ -13,11 +14,8 @@ class IdelloIE(InfoExtractor):
     IE_DESC = 'Idello.org'
     _LOGIN_REQUIRED = True
     _LOGIN_URL = 'https://www.idello.org/fr/api/v1/account/signin'
-    # _VALID_URL = r'https://www.idello.org/fr/ressource'
     _VALID_URL = r'https?://.+?\.idello\.org/fr/ressource/(?P<name_or_id>.+)\?navcontext=(?P<video_id>.+)'
     _NETRC_MACHINE = 'idello'
-    # _VALID_URL = r'https?://.+?\.idello\.org/(?:[^/]+/)?(?P<type>videos|show_videos|articles|feature|(?:[^/]+/\d+/video))(/.+)?/(?P<name_or_id>.+)'
-    #_VALID_URL = r'https?://medius.studios.ms/Embed/Video-nc/(?P<id>[A-Z0-9\-]+)'
 
     _TEST = {
         #'url': 'https://medius.studios.ms/Embed/Video-nc/B19-BDL2046?latestplayer=true',
@@ -41,9 +39,8 @@ class IdelloIE(InfoExtractor):
     }
 
     def _login(self):
-        print("about to login")
         username, password = self._get_login_info(netrc_machine=self._NETRC_MACHINE)
-        
+
         if username is None:
             if self._LOGIN_REQUIRED:
                 raise ExtractorError('No login info available, needed for using %s.' % self.IE_NAME, expected=True)
@@ -51,7 +48,7 @@ class IdelloIE(InfoExtractor):
 
         login_form_strs = {
             'username': username,
-            'password': hashlib.md5(password).hexdigest(),
+            'password': hashlib.md5(password.encode('ascii')).hexdigest(),
             "sigin_under_13": "false",
             "remember": 0
         }
@@ -88,15 +85,8 @@ class IdelloIE(InfoExtractor):
             raise ExtractorError('Unable to log in')
 
     def _find_video_id(self, webpage):
-        res_id = [
-            r'"video_id"\s*:\s*"(.*?)"',
-            r'class="hero-poster[^"]*?"[^>]*id="(.+?)"',
-            r'data-video-id="(.+?)"',
-            r'<object id="vid_(.+?)"',
-            r'<meta name="og:image" content=".*/(.+?)-(.+?)/.+.jpg"',
-            r'videoId&quot;\s*:\s*&quot;(.+?)&quot;',
-            r'videoId["\']\s*:\s*["\']([^"\']+?)["\']',
-        ]
+        res_id = [r'data-product-id="(.+?)"']
+
         return self._search_regex(res_id, webpage, 'video id', default=None)
 
     def _real_initialize(self):
@@ -105,18 +95,36 @@ class IdelloIE(InfoExtractor):
     def _real_extract(self, url):
         mobj = re.match(self._VALID_URL, url)
         video_id = mobj.group('video_id')
-        
         webpage = self._download_webpage(url, video_id)
-        print(webpage.encode('utf-8'))
         video_id = self._find_video_id(webpage)
-        return None
-        info_dict = self._extract_jwplayer_data(
-            webpage, video_id, require_title=False)
+        title = self._html_search_meta(['og:title'], webpage)
+        description = self._html_search_meta(['og:description'], webpage)
+        thumbnail = self._html_search_meta(['og:image'], webpage)
+        # season_number =  self._search_regex(r'<span[^>]+itemprop="partOfSeason">([^<]+)</span>', webpage, 'partOfSeason', default=None)
+        # episode_number = self._search_regex(r'<span[^>]+itemprop="episodeNumber">([^<]+)</span>', webpage, 'episodeNumber', default=None)
+        webpage = self._download_json('https://www.idello.org/fr/api/v1/video/get_infos', video_id, data=json.dumps({'product_id': video_id}).encode())
+        data = webpage.get("data")
+        formats = []
+        
+        for stream in data["streams"]:
+            formats.append({
+                'url': stream.get("master_playlist_url"),
+                'width': stream.get('width'),
+                'height': stream.get('height'),
+                'vcodec': stream.get('video_codec'),
+                'container': stream.get("container_type"),
+                'filesize': "size_in_bytes",
+                'ext': 'mp4'
+            })
 
-        info_dict.update({
+        self._sort_formats(formats)
+        
+        return {
             'id': video_id,
-            'title': self._og_search_title(webpage),
-            'description': self._og_search_description(webpage),
-        })
-
-        return self.playlist_result(entries)
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail,
+            'formats': formats,
+            # 'season_number': season_number,
+            # 'episode_number': episode_number
+        }
